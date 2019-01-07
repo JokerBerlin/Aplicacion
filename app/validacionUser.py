da628a789a2e9729aac9988799bd19d7bd75ecf6
blackadress <blackadress.01@gmail.com>
2019-01-07 10:25:55 -0500

    debugging ftw

--- app/validacionUser.py
+++ app/validacionUser.py
@@ -2,4 +2,5 @@ from app.models import Empleado
 
 def validacionUsuario(usuario):
     empleado = Empleado.objects.get(usuario=usuario)
+    print('perfil empleado: %s' % empleado.perfil)
     return empleado.perfil
